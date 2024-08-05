6921f96d28b84bf753e5698da6573d80296ebf1c
teebin2019 <teebin2019@gmail.com>
2024-08-05 19:36:53 +0700

    dd

--- database/connect.php
+++ database/connect.php
@@ -2,6 +2,6 @@
 $serverName = "localhost";
 $userName = "root";
 $userPassword = "";
-$dbName = "db_project1";
+$dbName = "db_project";
 
 $objCon = mysqli_connect($serverName, $userName, $userPassword, $dbName);
